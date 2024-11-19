33417ad19f6e627a498117dc9a0f1aa66dd0dcfe
Elijah Hampton <elijahhampton@MacBookPro.lan>
2024-11-18 23:21:09 -0500

    correct video in mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -270,8 +270,8 @@ const Home = () => {
                 key={index}
                 sx={{
                   position: 'relative',
-                  width: '100vw',
-                  height: '100vh',
+                  width: '100%',
+                  height: '100%',
                   overflow: 'hidden',
                 }}
               >
